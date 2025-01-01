a221ed18904fc7edc67acc8c21e0c8490923fdc4
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-01-01 21:39:01 +0900

    Add sorting feature for the products
    Sort by rating, popularity and price (high-to-low and low-to-high)

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,14 +1,17 @@
 import Card from "../components/Card";
 import styles from "./Shop.module.css";
+import { useState, useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import { increaseProductCount } from "../products";
+import { sortProducts } from "../utils/utilFunctions";
+
 // For the sake of using fetch every time we mount this component
 // even though we are fetching the same data that won't change
 // we fetch the data inside the Shop
-import { useEffect } from "react";
-import { useOutletContext } from "react-router-dom";
-import { increaseProductCount } from "../products";
-
+//
 function Shop() {
     const [products, setProducts] = useOutletContext();
+    const [valueSelect, setValueSelect] = useState("popularity");
 
     function handlerUpdateProducts(index, quantity) {
         const productsUpdate = products.slice();
@@ -43,17 +46,32 @@ function Shop() {
         };
     }, [products, setProducts]);
 
-    return products.length > 0 ? (
+    const productsSorted = sortProducts(products, valueSelect);
+
+    return productsSorted.length > 0 ? (
         <>
-            <div className={styles["cards-container"]}>
-                {products.map((product, index) => (
-                    <Card
-                        item={product}
-                        key={product.id}
-                        index={index}
-                        handlerUpdateProducts={handlerUpdateProducts}
-                    />
-                ))}
+            <div>
+                <div className={styles.sort}>
+                    <select
+                        onChange={(e) => setValueSelect(e.currentTarget.value)}
+                        value={valueSelect}
+                    >
+                        <option value="popularity">Popularity</option>
+                        <option value="rating">Rating</option>
+                        <option value="high-to-low">Price (High to Low)</option>
+                        <option value="low-to-high">Price (Low to High)</option>
+                    </select>
+                </div>
+                <div className={styles["cards-container"]}>
+                    {productsSorted.map((product, index) => (
+                        <Card
+                            item={product}
+                            key={product.id}
+                            index={index}
+                            handlerUpdateProducts={handlerUpdateProducts}
+                        />
+                    ))}
+                </div>
             </div>
         </>
     ) : (
---
+++ src/utils/utilFunctions.js
@@ -0,0 +1,32 @@
+function sortProducts(arr, type) {
+    const arrCopy = arr.slice();
+    switch (type) {
+        case "popularity":
+            arrCopy.sort(
+                (prodA, prodB) => -prodA.rating.count + prodB.rating.count,
+            );
+            break;
+
+        case "rating":
+            arrCopy.sort(
+                (prodA, prodB) => prodA.rating.rate - prodB.rating.rate,
+            );
+            break;
+
+        case "high-to-low":
+            arrCopy.sort((prodA, prodB) => -prodA.price + prodB.price);
+            break;
+
+        case "low-to-high":
+            arrCopy.sort((prodA, prodB) => prodA.price - prodB.price);
+            break;
+        default:
+            console.error("type was not given or had an incorrect type");
+            arrCopy.sort(
+                (prodA, prodB) => prodA.rating.count - prodB.rating.count,
+            );
+    }
+    return arrCopy;
+}
+
+export { sortProducts };
